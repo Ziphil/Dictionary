68d894310ae18ed3e54800134d4d0d025833d127
Ziphil <yfe23457@nifty.com>
2016-10-28 21:34:43 +0900

    環境設定ウィンドウのタイトルを変更

--- source/ziphil/controller/SettingController.groovy
+++ source/ziphil/controller/SettingController.groovy
@@ -25,7 +25,7 @@ import ziphil.module.Setting
 public class SettingController extends Controller<Boolean> {
 
   private static final String RESOURCE_PATH = "resource/fxml/setting.fxml"
-  private static final String TITLE = "設定"
+  private static final String TITLE = "環境設定"
   private static final Double DEFAULT_WIDTH = Measurement.rpx(640)
   private static final Double DEFAULT_HEIGHT = -1
 
