35ecef41d54a5b1ba1823729a9587e8e9a56b774
Ziphil <yfe23457@nifty.com>
2016-11-09 22:25:16 +0900

    パスワードによってデバッグ機能が使えるように変更

--- source/ziphil/Launcher.groovy
+++ source/ziphil/Launcher.groovy
@@ -10,7 +10,6 @@ public class Launcher {
   public static final String TITLE = "ZpDIC shalnif"
   public static final String VERSION = "1.0.0"
   public static final String DATE = "1742"
-  public static final Boolean DEBUG = false
   public static final String BASE_PATH = createBasePath()
 
   public static void main(String... args) {
