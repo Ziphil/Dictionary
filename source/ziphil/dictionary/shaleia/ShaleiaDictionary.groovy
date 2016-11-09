2a4e5b47ddcd51c9aa0edc25c8bd45987ab98382
Ziphil <yfe23457@nifty.com>
2016-11-09 19:39:59 +0900

    シャレイア語辞典形式でアルファベット順をファイルに保存するよう修正

--- source/ziphil/dictionary/shaleia/ShaleiaDictionary.groovy
+++ source/ziphil/dictionary/shaleia/ShaleiaDictionary.groovy
@@ -164,6 +164,8 @@ public class ShaleiaDictionary extends Dictionary<ShaleiaWord, ShaleiaSuggestion
         output.append("* ").append(word.getUniqueName()).append("\n")
         output.append(word.getData().trim()).append("\n\n")
       }
+      output.append("* META-ALPHABET-ORDER\n\n")
+      output.append($alphabetOrder).append("\n\n")
       output.append("* META-CHANGE\n\n")
       output.append($changeData.trim()).append("\n\n")
       file.setText(output.toString(), "UTF-8")
