1c3b4dac9dfeecd473c14e503a8ea9e60e4e9da0
Ziphil <yfe23457@nifty.com>
2017-02-01 03:22:39 +0900

    リファクタリング

--- source/ziphil/dictionary/shaleia/ShaleiaWord.groovy
+++ source/ziphil/dictionary/shaleia/ShaleiaWord.groovy
@@ -23,7 +23,7 @@ public class ShaleiaWord extends WordBase {
   }
 
   private void updateName() {
-    $name = (uniqueName.startsWith("\$")) ? "" : uniqueName.replaceAll(/\+|~/, "")
+    $name = ($uniqueName.startsWith("\$")) ? "" : $uniqueName.replaceAll(/\+|~/, "")
   }
 
   private void updateEquivalents() {
