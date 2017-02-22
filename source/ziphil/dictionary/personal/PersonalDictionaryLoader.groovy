7e8ef6fcf1348307daa647e660562f548d8ea302
Ziphil <yfe23457@nifty.com>
2017-02-22 21:55:28 +0900

    PDIC形式の読み込みを文字単位で行うよう修正

--- source/ziphil/dictionary/personal/PersonalDictionaryLoader.groovy
+++ source/ziphil/dictionary/personal/PersonalDictionaryLoader.groovy
@@ -10,6 +10,11 @@ import ziphilib.transform.Ziphilify
 @CompileStatic @Ziphilify
 public class PersonalDictionaryLoader extends DictionaryLoader<PersonalDictionary, PersonalWord> {
 
+  private static final Integer QUOTATION = 34
+  private static final Integer COMMA = 44
+  private static final Integer LINE_BREAK = 10
+  private static final Integer CARRIAGE_RETURN = 13
+
   public PersonalDictionaryLoader(PersonalDictionary dictionary, String path) {
     super(dictionary, path)
   }
@@ -17,25 +22,137 @@ public class PersonalDictionaryLoader extends DictionaryLoader<PersonalDictionar
   protected ObservableList<PersonalWord> call() {
     if ($path != null) {
       File file = File.new($path)
-      String input = file.getText().replaceAll(/\r/, "")
-      Matcher matcher = input =~ /(?s)"(.*?)","(.*?)","(.*?)",(\d*?),(\d*?),(\d*?),"(.*?)"/
-      while (matcher.find()) {
-        if (isCancelled()) {
-          return null
+      BufferedReader reader = file.newReader()
+      PersonalWord word = PersonalWord.new()
+      StringBuilder currentValue = StringBuilder.new()
+      Map<Integer, String> headerData = HashMap.new()
+      Integer index = 0
+      Boolean isFirstLine = true
+      Boolean isReadingValue = false
+      Boolean isQuoted = false
+      try {
+        for (Integer codePoint = -1 ; (codePoint = reader.read()) != -1 ;) {
+          if (codePoint == CARRIAGE_RETURN) {
+            continue
+          }
+          if (isReadingValue) {
+            if (isQuoted) {
+              if (codePoint == QUOTATION) {
+                Integer nextCodePoint = reader.read()
+                if (nextCodePoint == QUOTATION) {
+                  currentValue.appendCodePoint(codePoint)
+                } else if (nextCodePoint == COMMA) {
+                  if (isFirstLine) {
+                    fillHeaderData(headerData, index, currentValue)
+                  } else {
+                    fillWord(word, headerData, index, currentValue)
+                  }
+                  currentValue.setLength(0)
+                  index ++
+                  isReadingValue = false
+                } else if (nextCodePoint == LINE_BREAK || nextCodePoint == -1) {
+                  if (isFirstLine) {
+                    fillHeaderData(headerData, index, currentValue)
+                  } else {
+                    fillWord(word, headerData, index, currentValue)
+                    word.setDictionary($dictionary)
+                    word.update()
+                    $words.add(word)
+                  }
+                  word = PersonalWord.new()
+                  currentValue.setLength(0)
+                  index = 0
+                  isReadingValue = false
+                  isFirstLine = false
+                }
+              } else {
+                currentValue.appendCodePoint(codePoint)
+              }
+            } else {
+              if (codePoint == COMMA) {
+                if (isFirstLine) {
+                  fillHeaderData(headerData, index, currentValue)
+                } else {
+                  fillWord(word, headerData, index, currentValue)
+                }
+                currentValue.setLength(0)
+                index ++
+                isReadingValue = false
+              } else if (codePoint == LINE_BREAK) {
+                if (isFirstLine) {
+                  fillHeaderData(headerData, index, currentValue)
+                } else {
+                  fillWord(word, headerData, index, currentValue)
+                  word.setDictionary($dictionary)
+                  word.update()
+                  $words.add(word)
+                }
+                word = PersonalWord.new()
+                currentValue.setLength(0)
+                index = 0
+                isReadingValue = false
+                isFirstLine = false
+              } else {
+                currentValue.appendCodePoint(codePoint)
+              }
+            }
+          } else {
+            if (codePoint == QUOTATION) {
+              isReadingValue = true
+              isQuoted = true
+            } else if (codePoint == COMMA) {
+              currentValue.setLength(0)
+              index ++
+            } else if (codePoint == LINE_BREAK) {
+              word = PersonalWord.new()
+              currentValue.setLength(0)
+              index = 0
+              isFirstLine = false
+            } else {
+              currentValue.appendCodePoint(codePoint)
+              isReadingValue = true
+              isQuoted = false
+            }
+          }
         }
-        PersonalWord word = PersonalWord.new()
-        word.setName(matcher.group(1))
-        word.setPronunciation(matcher.group(7))
-        word.setTranslation(matcher.group(2))
-        word.setUsage(matcher.group(3))
-        word.setLevel(matcher.group(4).toInteger())
-        word.setMemory(matcher.group(5).toInteger())
-        word.setModification(matcher.group(6).toInteger())
-        word.update()
-        $words.add(word)
+        if (isReadingValue) {
+          if (isFirstLine) {
+            fillHeaderData(headerData, index, currentValue)
+          } else {
+            fillWord(word, headerData, index, currentValue)
+            word.setDictionary($dictionary)
+            word.update()
+            $words.add(word)
+          }
+        }
+      } finally {
+        reader.close()
       }
     }
     return $words
+  }
+
+  private void fillHeaderData(Map<Integer, String> headerData, Integer index, StringBuilder currentValue) {
+    headerData.put(index, currentValue.toString())
+  }
+
+  private void fillWord(PersonalWord word, Map<Integer, String> headerData, Integer index, StringBuilder currentValue) {
+    String mode = headerData[index]
+    if (mode == "word") {
+      word.setName(currentValue.toString())
+    } else if (mode == "pron") {
+      word.setPronunciation(currentValue.toString())
+    } else if (mode == "trans") {
+      word.setTranslation(currentValue.toString())
+    } else if (mode == "exp") {
+      word.setUsage(currentValue.toString())
+    } else if (mode == "level") {
+      word.setLevel(currentValue.toString().toInteger())
+    } else if (mode == "memory") {
+      word.setLevel(currentValue.toString().toInteger())
+    } else if (mode == "modify") {
+      word.setModification(currentValue.toString().toInteger())
+    }
   }
 
 }
