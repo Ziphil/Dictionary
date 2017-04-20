d63f644a414a17d8e2bcf46054f1e16e1df0e895
Ziphil <yfe23457@nifty.com>
2017-04-20 21:08:57 +0900

    PDIC形式で発音記号が登録されている場合はそれを表示するよう変更

--- source/ziphil/dictionary/personal/PersonalWordContentPaneFactory.groovy
+++ source/ziphil/dictionary/personal/PersonalWordContentPaneFactory.groovy
@@ -14,6 +14,8 @@ import ziphilib.transform.Ziphilify
 @CompileStatic @Ziphilify
 public class PersonalWordContentPaneFactory extends ContentPaneFactoryBase<PersonalWord, PersonalDictionary> {
 
+  private static final String PERSONAL_PRONUNCIATION_CLASS = "persoanl-pronunciation"
+
   public PersonalWordContentPaneFactory(PersonalWord word, PersonalDictionary dictionary) {
     super(word, dictionary)
   }
@@ -23,18 +25,33 @@ public class PersonalWordContentPaneFactory extends ContentPaneFactoryBase<Perso
     TextFlow contentPane = TextFlow.new()
     contentPane.getStyleClass().add(CONTENT_PANE_CLASS)
     contentPane.setLineSpacing(lineSpacing)
-    addNameNode(contentPane, $word.getName())
+    addNameNode(contentPane, $word.getName(), $word.getPronunciation())
     addOtherNode(contentPane, $word.getTranslation())
     addOtherNode(contentPane, $word.getUsage())
     modifyBreak(contentPane)
     return contentPane
   }
 
-  private void addNameNode(TextFlow contentPane, String name) {
-    Label nameText = Label.new(name)
-    Text breakText = Text.new("\n")
-    nameText.getStyleClass().addAll(CONTENT_CLASS, HEAD_NAME_CLASS)
-    contentPane.getChildren().addAll(nameText, breakText)
+  private void addNameNode(TextFlow contentPane, String name, String pronunciation) {
+    if (pronunciation != "") {
+      if (!pronunciation.startsWith("/") && !pronunciation.startsWith("[")) {
+        pronunciation = "/" + pronunciation
+      }
+      if (!pronunciation.endsWith("/") && !pronunciation.endsWith("[")) {
+        pronunciation = pronunciation + "/"
+      }
+      Label nameText = Label.new(name + " ")
+      Text pronunciationText = Text.new(pronunciation)
+      Text breakText = Text.new("\n")
+      nameText.getStyleClass().addAll(CONTENT_CLASS, HEAD_NAME_CLASS)
+      pronunciationText.getStyleClass().addAll(CONTENT_CLASS, PERSONAL_PRONUNCIATION_CLASS)
+      contentPane.getChildren().addAll(nameText, pronunciationText, breakText)
+    } else {
+      Label nameText = Label.new(name)
+      Text breakText = Text.new("\n")
+      nameText.getStyleClass().addAll(CONTENT_CLASS, HEAD_NAME_CLASS)
+      contentPane.getChildren().addAll(nameText, breakText)
+    }
   }
 
   private void addOtherNode(TextFlow contentPane, String other) {
