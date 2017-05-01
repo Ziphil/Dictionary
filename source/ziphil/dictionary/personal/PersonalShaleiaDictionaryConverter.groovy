dc4a292fa4867ae31705428e27d12a63e7fc24e0
Ziphil <yfe23457@nifty.com>
2017-05-01 10:33:13 +0900

    リファクタリング

--- source/ziphil/dictionary/personal/PersonalShaleiaDictionaryConverter.groovy
+++ source/ziphil/dictionary/personal/PersonalShaleiaDictionaryConverter.groovy
@@ -4,6 +4,7 @@ import groovy.transform.CompileStatic
 import java.util.regex.Matcher
 import ziphil.dictionary.DictionaryConverter
 import ziphil.dictionary.Word
+import ziphil.dictionary.shaleia.ShaleiaDescriptionReader
 import ziphil.dictionary.shaleia.ShaleiaDictionary
 import ziphil.dictionary.shaleia.ShaleiaWord
 import ziphilib.transform.Ziphilify
@@ -29,58 +30,25 @@ public class PersonalShaleiaDictionaryConverter extends DictionaryConverter<Pers
         newWord.setName(oldWord.getName())
         StringBuilder newTranslation = StringBuilder.new()
         StringBuilder newUsage = StringBuilder.new()
-        BufferedReader oldDescriptionReader = BufferedReader.new(StringReader.new(oldWord.getDescription()))
-        for (String line ; (line = oldDescriptionReader.readLine()) != null ;) {
-          Matcher creationDateMatcher = line =~ /^\+\s*(\d+)(?:\s*〈(.+)〉)?\s*$/
-          Matcher equivalentMatcher = line =~ /^\=\s*〈(.+)〉\s*(.+)$/
-          Matcher meaningMatcher = line =~ /^M>\s*(.+)$/
-          Matcher etymologyMatcher = line =~ /^E>\s*(.+)$/
-          Matcher usageMatcher = line =~ /^U>\s*(.+)$/
-          Matcher phraseMatcher = line =~ /^P>\s*(.+)$/
-          Matcher noteMatcher = line =~ /^N>\s*(.+)$/
-          Matcher taskMatcher = line =~ /^O>\s*(.+)$/
-          Matcher exampleMatcher = line =~ /^S>\s*(.+)$/
-          Matcher synonymMatcher = line =~ /^\-\s*(.+)$/
-          if (creationDateMatcher.matches()) {
-            String creationDate = creationDateMatcher.group(1)
-            String totalPart = creationDateMatcher.group(2)
+        ShaleiaDescriptionReader oldReader = ShaleiaDescriptionReader.new(oldWord.getDescription())
+        while (oldReader.readLine() != null) {
+          if (oldReader.findCreationDate()) {
+            String creationDate = oldReader.lookupCreationDate()
+            String totalPart = oldReader.lookupTotalPart()
             appendCreationDate(newTranslation, totalPart, creationDate)
           }
-          if (equivalentMatcher.matches()) {
-            String part = equivalentMatcher.group(1)
-            String equivalent = equivalentMatcher.group(2)
+          if (oldReader.findEquivalent()) {
+            String part = oldReader.lookupPart()
+            String equivalent = oldReader.lookupEquivalent()
             appendEquivalent(newTranslation, part, equivalent)
           }
-          if (meaningMatcher.matches()) {
-            String meaning = meaningMatcher.group(1)
-            appendOther(newUsage, "語義", meaning)
+          if (oldReader.findContent()) {
+            String title = oldReader.title()
+            String content = oldReader.lookupContent()
+            appendContent(newUsage, title, content)
           }
-          if (etymologyMatcher.matches()) {
-            String etymology = etymologyMatcher.group(1)
-            appendOther(newUsage, "語源", etymology)
-          }
-          if (usageMatcher.matches()) {
-            String usage = usageMatcher.group(1)
-            appendOther(newUsage, "語法", usage)
-          }
-          if (phraseMatcher.matches()) {
-            String phrase = phraseMatcher.group(1)
-            appendOther(newUsage, "成句", phrase)
-          }
-          if (noteMatcher.matches()) {
-            String note = noteMatcher.group(1)
-            appendOther(newUsage, "備考", note)
-          }
-          if (taskMatcher.matches()) {
-            String task = taskMatcher.group(1)
-            appendOther(newUsage, "タスク", task)
-          }
-          if (exampleMatcher.matches()) {
-            String example = exampleMatcher.group(1)
-            appendOther(newUsage, "例文", example)
-          }
-          if (synonymMatcher.matches()) {
-            String synonym = synonymMatcher.group(1)
+          if (oldReader.findSynonym()) {
+            String synonym = oldReader.lookupSynonym()
             appendSynonym(newUsage, synonym)
           }
         }
@@ -112,11 +80,11 @@ public class PersonalShaleiaDictionaryConverter extends DictionaryConverter<Pers
     newTranslation.append("\n")
   }
 
-  private void appendOther(StringBuilder newUsage, String title, String other) {
+  private void appendContent(StringBuilder newUsage, String title, String content) {
     newUsage.append("【")
     newUsage.append(title)
     newUsage.append("】\n")
-    newUsage.append(other.replaceAll(/(\{|\}|\[|\]|\/)/, ""))
+    newUsage.append(content.replaceAll(/(\{|\}|\[|\]|\/)/, ""))
     newUsage.append("\n")
   }
 
--- source/ziphil/dictionary/slime/SlimeShaleiaDictionaryConverter.groovy
+++ source/ziphil/dictionary/slime/SlimeShaleiaDictionaryConverter.groovy
@@ -4,6 +4,7 @@ import groovy.transform.CompileStatic
 import java.util.regex.Matcher
 import ziphil.dictionary.DictionaryConverter
 import ziphil.dictionary.Word
+import ziphil.dictionary.shaleia.ShaleiaDescriptionReader
 import ziphil.dictionary.shaleia.ShaleiaDictionary
 import ziphil.dictionary.shaleia.ShaleiaWord
 import ziphilib.transform.Ziphilify
@@ -28,48 +29,17 @@ public class SlimeShaleiaDictionaryConverter extends DictionaryConverter<SlimeDi
         SlimeWord newWord = SlimeWord.new()
         newWord.setId(i + 1)
         newWord.setName(oldWord.getName())
-        BufferedReader oldDescriptionReader = BufferedReader.new(StringReader.new(oldWord.getDescription()))
-        for (String line ; (line = oldDescriptionReader.readLine()) != null ;) {
-          Matcher equivalentMatcher = line =~ /^\=\s*〈(.+)〉\s*(.+)$/
-          Matcher meaningMatcher = line =~ /^M>\s*(.+)$/
-          Matcher etymologyMatcher = line =~ /^E>\s*(.+)$/
-          Matcher usageMatcher = line =~ /^U>\s*(.+)$/
-          Matcher phraseMatcher = line =~ /^P>\s*(.+)$/
-          Matcher noteMatcher = line =~ /^N>\s*(.+)$/
-          Matcher taskMatcher = line =~ /^O>\s*(.+)$/
-          Matcher exampleMatcher = line =~ /^S>\s*(.+)$/
-          if (equivalentMatcher.matches()) {
-            String part = equivalentMatcher.group(1)
-            String equivalent = equivalentMatcher.group(2)
+        ShaleiaDescriptionReader oldReader = ShaleiaDescriptionReader.new(oldWord.getDescription())
+        while (oldReader.readLine() != null) {
+          if (oldReader.findEquivalent()) {
+            String part = oldReader.lookupPart()
+            String equivalent = oldReader.lookupEquivalent()
             addEquivalent(newWord, part, equivalent)
           }
-          if (meaningMatcher.matches()) {
-            String meaning = meaningMatcher.group(1)
-            addOther(newWord, "語義", meaning)
-          }
-          if (etymologyMatcher.matches()) {
-            String etymology = etymologyMatcher.group(1)
-            addOther(newWord, "語源", etymology)
-          }
-          if (usageMatcher.matches()) {
-            String usage = usageMatcher.group(1)
-            addOther(newWord, "語法", usage)
-          }
-          if (phraseMatcher.matches()) {
-            String phrase = phraseMatcher.group(1)
-            addOther(newWord, "成句", phrase)
-          }
-          if (noteMatcher.matches()) {
-            String note = noteMatcher.group(1)
-            addOther(newWord, "備考", note)
-          }
-          if (taskMatcher.matches()) {
-            String task = taskMatcher.group(1)
-            addOther(newWord, "タスク", task)
-          }
-          if (exampleMatcher.matches()) {
-            String example = exampleMatcher.group(1)
-            addOther(newWord, "例文", example)
+          if (oldReader.findContent()) {
+            String title = oldReader.title()
+            String content = oldReader.lookupContent()
+            addContent(newWord, title, content)
           }
         }
         newWord.setDictionary($newDictionary)
@@ -87,10 +57,10 @@ public class SlimeShaleiaDictionaryConverter extends DictionaryConverter<SlimeDi
     newWord.getRawEquivalents().add(newEquivalent)
   }
 
-  private void addOther(SlimeWord newWord, String title, String other) {
+  private void addContent(SlimeWord newWord, String title, String content) {
     SlimeInformation newInformation = SlimeInformation.new()
     newInformation.setTitle(title)
-    newInformation.setText(other.replaceAll(/(\{|\}|\[|\]|\/)/, ""))
+    newInformation.setText(content.replaceAll(/(\{|\}|\[|\]|\/)/, ""))
     newWord.getInformations().add(newInformation)
   }
 
