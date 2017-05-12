7904b93c20cc02af907500843541a804665095d0
Ziphil <yfe23457@nifty.com>
2017-05-12 22:24:26 +0900

    文字頻度のウィンドウサイズのデフォルト値を変更

--- source/ziphil/controller/CharacterFrequencyController.groovy
+++ source/ziphil/controller/CharacterFrequencyController.groovy
@@ -22,7 +22,7 @@ public class CharacterFrequencyController extends Controller<Void> {
   private static final String RESOURCE_PATH = "resource/fxml/controller/character_frequency.fxml"
   private static final String TITLE = "文字頻度"
   private static final Double DEFAULT_WIDTH = Measurement.rpx(480)
-  private static final Double DEFAULT_HEIGHT = Measurement.rpx(320)
+  private static final Double DEFAULT_HEIGHT = Measurement.rpx(370)
   protected static final Integer MAX_PIE_SIZE = 20
 
   @FXML private PieChart $frequencyChart
