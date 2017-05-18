b7136fa103fe86883f3f933f05f55923a1bbc11a
Ziphil <yfe23457@nifty.com>
2017-05-19 02:30:22 +0900

    PDIC形式の読み込みをCSVライブラリを用いる方式に変更

--- source/ziphil/dictionary/personal/PersonalDictionaryLoader.groovy
+++ source/ziphil/dictionary/personal/PersonalDictionaryLoader.groovy
@@ -1,5 +1,8 @@
 package ziphil.dictionary.personal
 
+import com.orangesignal.csv.CsvConfig
+import com.orangesignal.csv.CsvReader
+import com.orangesignal.csv.io.CsvColumnNameMapReader
 import groovy.transform.CompileStatic
 import java.util.regex.Matcher
 import javafx.collections.ObservableList
@@ -17,104 +20,20 @@ public class PersonalDictionaryLoader extends DictionaryLoader<PersonalDictionar
   protected Boolean load() {
     File file = File.new($path)
     BufferedReader reader = file.newReader()
-    PersonalWord word = PersonalWord.new()
-    StringBuilder currentValue = StringBuilder.new()
-    Map<Integer, String> headerData = HashMap.new()
-    Integer index = 0
-    Boolean isFirstLine = true
-    Boolean isReadingValue = false
-    Boolean isQuoted = false
+    CsvConfig config = CsvConfig.new()
+    config.setQuoteDisabled(false)
+    config.setEscapeDisabled(false)
+    config.setEscape((Character)'"')
+    config.setIgnoreEmptyLines(true)
+    CsvColumnNameMapReader csvReader = CsvColumnNameMapReader.new(CsvReader.new(reader, config))
     try {
-      for (Integer codePoint = -1 ; (codePoint = reader.read()) != -1 ;) {
-        if (codePoint == '\r') {
-          continue
+      for (Map<String, String> line ; (line = csvReader.read()) != null ;) {
+        if (isCancelled()) {
+          return false
         }
-        if (isReadingValue) {
-          if (isQuoted) {
-            if (codePoint == '"') {
-              Integer nextCodePoint = reader.read()
-              if (nextCodePoint == '"') {
-                currentValue.appendCodePoint(codePoint)
-              } else if (nextCodePoint == ',') {
-                if (isFirstLine) {
-                  fillHeaderData(headerData, index, currentValue)
-                } else {
-                  fillWord(word, headerData, index, currentValue)
-                }
-                currentValue.setLength(0)
-                index ++
-                isReadingValue = false
-              } else if (nextCodePoint == '\n' || nextCodePoint == -1) {
-                if (isFirstLine) {
-                  fillHeaderData(headerData, index, currentValue)
-                } else {
-                  fillWord(word, headerData, index, currentValue)
-                  word.setDictionary($dictionary)
-                  $words.add(word)
-                }
-                word = PersonalWord.new()
-                currentValue.setLength(0)
-                index = 0
-                isReadingValue = false
-                isFirstLine = false
-              }
-            } else {
-              currentValue.appendCodePoint(codePoint)
-            }
-          } else {
-            if (codePoint == ',') {
-              if (isFirstLine) {
-                fillHeaderData(headerData, index, currentValue)
-              } else {
-                fillWord(word, headerData, index, currentValue)
-              }
-              currentValue.setLength(0)
-              index ++
-              isReadingValue = false
-            } else if (codePoint == '\n') {
-              if (isFirstLine) {
-                fillHeaderData(headerData, index, currentValue)
-              } else {
-                fillWord(word, headerData, index, currentValue)
-                word.setDictionary($dictionary)
-                $words.add(word)
-              }
-              word = PersonalWord.new()
-              currentValue.setLength(0)
-              index = 0
-              isReadingValue = false
-              isFirstLine = false
-            } else {
-              currentValue.appendCodePoint(codePoint)
-            }
-          }
-        } else {
-          if (codePoint == '"') {
-            isReadingValue = true
-            isQuoted = true
-          } else if (codePoint == ',') {
-            currentValue.setLength(0)
-            index ++
-          } else if (codePoint == '\n') {
-            word = PersonalWord.new()
-            currentValue.setLength(0)
-            index = 0
-            isFirstLine = false
-          } else {
-            currentValue.appendCodePoint(codePoint)
-            isReadingValue = true
-            isQuoted = false
-          }
-        }
-      }
-      if (isReadingValue) {
-        if (isFirstLine) {
-          fillHeaderData(headerData, index, currentValue)
-        } else {
-          fillWord(word, headerData, index, currentValue)
-          word.setDictionary($dictionary)
-          $words.add(word)
-        }
+        PersonalWord word = PersonalWord.new()
+        fillWord(word, line)
+        $words.add(word)
       }
     } finally {
       reader.close()
@@ -122,26 +41,34 @@ public class PersonalDictionaryLoader extends DictionaryLoader<PersonalDictionar
     return true
   }
 
-  private void fillHeaderData(Map<Integer, String> headerData, Integer index, StringBuilder currentValue) {
-    headerData.put(index, currentValue.toString())
-  }
-
-  private void fillWord(PersonalWord word, Map<Integer, String> headerData, Integer index, StringBuilder currentValue) {
-    String mode = headerData[index]
-    if (mode == "word") {
-      word.setName(currentValue.toString())
-    } else if (mode == "pron") {
-      word.setPronunciation(currentValue.toString())
-    } else if (mode == "trans") {
-      word.setTranslation(currentValue.toString())
-    } else if (mode == "exp") {
-      word.setUsage(currentValue.toString())
-    } else if (mode == "level") {
-      word.setLevel(currentValue.toString().toInteger())
-    } else if (mode == "memory") {
-      word.setLevel(currentValue.toString().toInteger())
-    } else if (mode == "modify") {
-      word.setModification(currentValue.toString().toInteger())
+  private void fillWord(PersonalWord word, Map<String, String> line) {
+    String name = line["word"]
+    String pronunciation = line["pron"]
+    String translation = line["trans"]
+    String usage = line["exp"]
+    String level = line["level"]
+    String memory = line["memory"]
+    String modification = line["modify"]
+    if (name != null) {
+      word.setName(name)
+    }
+    if (pronunciation != null) {
+      word.setPronunciation(pronunciation)
+    }
+    if (translation != null) {
+      word.setTranslation(translation)
+    }
+    if (usage != null) {
+      word.setUsage(usage)
+    }
+    if (level != null) {
+      word.setLevel(level.toInteger())
+    }
+    if (memory != null) {
+      word.setMemory(memory.toInteger())
+    }
+    if (modification != null) {
+      word.setModification(modification.toInteger())
     }
   }
 
