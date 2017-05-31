3ba5f694e4b2a61c386577d164b566052872d8b9
Ziphil <yfe23457@nifty.com>
2017-05-31 21:39:14 +0900

    文一括検索の結果を表示するウィンドウとそのコントローラを作成

---
+++ source/ziphil/controller/SentenceSearchResultController.groovy
@@ -0,0 +1,32 @@
+package ziphil.controller
+
+import groovy.transform.CompileStatic
+import javafx.collections.ObservableList
+import javafx.fxml.FXML
+import javafx.scene.control.ListView
+import ziphil.custom.Measurement
+import ziphil.custom.UtilityStage
+import ziphil.dictionary.Word
+import ziphilib.transform.Ziphilify
+
+
+@CompileStatic @Ziphilify
+public class SentenceSearchResultController extends Controller<Void> {
+
+  private static final String RESOURCE_PATH = "resource/fxml/controller/sentence_search_result.fxml"
+  private static final String TITLE = "文一括検索結果"
+  private static final Double DEFAULT_WIDTH = Measurement.rpx(480)
+  private static final Double DEFAULT_HEIGHT = Measurement.rpx(320)
+
+  @FXML private ListView<List<Word>> $resultView
+
+  public SentenceSearchResultController(UtilityStage<Void> stage) {
+    super(stage)
+    loadResource(RESOURCE_PATH, TITLE, DEFAULT_WIDTH, DEFAULT_HEIGHT, true)
+  }
+
+  public void prepare(ObservableList<List<Word>> result) {
+    $resultView.setItems(result)
+  }
+
+}
--- source/ziphil/controller/SentenceSearcherController.groovy
+++ source/ziphil/controller/SentenceSearcherController.groovy
@@ -1,8 +1,12 @@
 package ziphil.controller
 
 import groovy.transform.CompileStatic
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import javafx.fxml.FXML
 import javafx.scene.control.TextField
+import javafx.stage.StageStyle
+import javafx.stage.Modality
 import ziphil.custom.Measurement
 import ziphil.custom.UtilityStage
 import ziphil.dictionary.Dictionary
@@ -36,6 +40,7 @@ public class SentenceSearcherController extends Controller<Void> {
   private void search() {
     String sentence = $sentenceControl.getText()
     String punctuations = $punctuationsControl.getText()
+    ObservableList<List<Word>> result = FXCollections.observableArrayList()
     for (String wordName : sentence.split(/\s+/)) {
       StringBuilder nextWordName = StringBuilder.new()
       for (String character : wordName) {
@@ -45,11 +50,20 @@ public class SentenceSearcherController extends Controller<Void> {
       }
       $dictionary.searchByName(nextWordName.toString(), true, true)
       List<Element> hitWords = $dictionary.getWholeWords()
+      List<Word> finalHitwords = ArrayList.new()
       for (Element word : hitWords) {
         if (word instanceof Word) {
+          finalHitwords.add((Word)word)
         }
       }
+      result.add(finalHitwords)
     }
+    UtilityStage<Void> nextStage = UtilityStage.new(StageStyle.UTILITY)
+    SentenceSearchResultController controller = SentenceSearchResultController.new(nextStage)
+    nextStage.initModality(Modality.APPLICATION_MODAL)
+    nextStage.initOwner($stage)
+    controller.prepare(result)
+    nextStage.showAndWait()
   }
 
   @FXML
