0aa8bdd44d4a03483ba0f7d00791b32a6350c0d7
Ziphil <yfe23457@nifty.com>
2017-06-01 18:07:14 +0900

    リファクタリング

--- source/ziphil/controller/SentenceSearcherController.groovy
+++ source/ziphil/controller/SentenceSearcherController.groovy
@@ -1,8 +1,6 @@
 package ziphil.controller
 
 import groovy.transform.CompileStatic
-import javafx.collections.FXCollections
-import javafx.collections.ObservableList
 import javafx.fxml.FXML
 import javafx.scene.control.TextField
 import javafx.stage.StageStyle
@@ -10,18 +8,7 @@ import javafx.stage.Modality
 import ziphil.custom.Measurement
 import ziphil.custom.UtilityStage
 import ziphil.dictionary.Dictionary
-import ziphil.dictionary.Element
-import ziphil.dictionary.NormalSearchParameter
-import ziphil.dictionary.SearchMode
-import ziphil.dictionary.Suggestion
-import ziphil.dictionary.Word
-import ziphil.dictionary.shaleia.ShaleiaDictionary
-import ziphil.dictionary.shaleia.ShaleiaPossibility
-import ziphil.dictionary.shaleia.ShaleiaSuggestion
-import ziphil.dictionary.slime.SlimeDictionary
-import ziphil.dictionary.slime.SlimePossibility
-import ziphil.dictionary.slime.SlimeSearchParameter
-import ziphil.dictionary.slime.SlimeSuggestion
+import ziphil.dictionary.SentenceSearcher
 import ziphilib.transform.Ziphilify
 
 
@@ -50,59 +37,12 @@ public class SentenceSearcherController extends Controller<Void> {
   private void search() {
     String sentence = $sentenceControl.getText()
     String punctuations = $punctuationsControl.getText()
-    ObservableList<List<Word>> result = FXCollections.observableArrayList()
-    for (String wordName : sentence.split(/\s+/)) {
-      StringBuilder nextWordName = StringBuilder.new()
-      for (String character : wordName) {
-        if (punctuations.indexOf(character) < 0) {
-          nextWordName.append(character)
-        }
-      }
-      if (nextWordName.length() > 0) {
-        NormalSearchParameter parameter = NormalSearchParameter.new(nextWordName.toString(), SearchMode.NAME, true, true)
-        $dictionary.searchNormal(parameter)
-        List<Element> hitWords = ArrayList.new($dictionary.getWholeWords())
-        List<Word> finalHitwords = ArrayList.new()
-        for (Element word : hitWords) {
-          if (word instanceof Word) {
-            finalHitwords.add((Word)word)
-          } else {
-            if ($dictionary instanceof ShaleiaDictionary && word instanceof ShaleiaSuggestion) {
-              List<ShaleiaPossibility> possibilities = ArrayList.new(((ShaleiaSuggestion)word).getPossibilities())
-              for (ShaleiaPossibility possibility : possibilities) {
-                NormalSearchParameter suggestionParameter = NormalSearchParameter.new(possibility.getName(), SearchMode.NAME, true, true)
-                $dictionary.searchNormal(suggestionParameter)
-                List<Element> suggestionHitWords = $dictionary.getWholeWords()
-                for (Element suggestionWord : suggestionHitWords) {
-                  if (suggestionWord instanceof Word) {
-                    finalHitwords.add((Word)suggestionWord)
-                  }
-                }
-              }
-            } else if ($dictionary instanceof SlimeDictionary && word instanceof SlimeSuggestion) {
-              List<SlimePossibility> possibilities = ArrayList.new(((SlimeSuggestion)word).getPossibilities())
-              for (SlimePossibility possibility : possibilities) {
-                SlimeSearchParameter suggestionParameter = SlimeSearchParameter.new()
-                suggestionParameter.setId(possibility.getWord().getId())
-                $dictionary.searchDetail(suggestionParameter)
-                List<Element> suggestionHitWords = $dictionary.getWholeWords()
-                for (Element suggestionWord : suggestionHitWords) {
-                  if (suggestionWord instanceof Word) {
-                    finalHitwords.add((Word)suggestionWord)
-                  }
-                }
-              }
-            }
-          }
-        }
-        result.add(finalHitwords)
-      }
-    }
+    SentenceSearcher searcher = SentenceSearcher.new($dictionary, sentence, punctuations)
     UtilityStage<Void> nextStage = UtilityStage.new(StageStyle.UTILITY)
     SentenceSearchResultController controller = SentenceSearchResultController.new(nextStage)
     nextStage.initModality(Modality.APPLICATION_MODAL)
     nextStage.initOwner($stage)
-    controller.prepare(result)
+    controller.prepare(searcher.search())
     nextStage.showAndWait()
   }
 
