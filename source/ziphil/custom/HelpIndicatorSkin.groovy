21e5dee0da87ae9849b5e2fd17e8044efd405c46
Ziphil <yfe23457@nifty.com>
2017-06-10 22:20:22 +0900

    HelpIndicatorの実際の大きさが見た目よりも大きくなっていたのを修正

--- source/ziphil/custom/HelpIndicatorSkin.groovy
+++ source/ziphil/custom/HelpIndicatorSkin.groovy
@@ -3,15 +3,15 @@ package ziphil.custom
 import groovy.transform.CompileStatic
 import javafx.collections.ObservableList
 import javafx.fxml.FXML
+import javafx.scene.Group
 import javafx.scene.control.Label
 import javafx.scene.control.Tooltip
-import javafx.scene.layout.HBox
 import javafx.scene.layout.StackPane
 import ziphilib.transform.Ziphilify
 
 
 @CompileStatic @Ziphilify
-public class HelpIndicatorSkin extends CustomSkinBase<HelpIndicator, HBox> {
+public class HelpIndicatorSkin extends CustomSkinBase<HelpIndicator, Group> {
 
   private static final String RESOURCE_PATH = "resource/fxml/custom/help_indicator.fxml"
 
@@ -20,7 +20,7 @@ public class HelpIndicatorSkin extends CustomSkinBase<HelpIndicator, HBox> {
 
   public HelpIndicatorSkin(HelpIndicator control) {
     super(control)
-    $node = HBox.new()
+    $node = Group.new()
     loadResource(RESOURCE_PATH)
     setupNode()
   }
