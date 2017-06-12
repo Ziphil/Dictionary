14a22556c5b38a714214b765bebac96f83e1e459
Ziphil <yfe23457@nifty.com>
2017-06-13 03:08:41 +0900

    印刷画面から実際に単語データを印刷できるよう変更

--- source/ziphil/controller/PrintController.groovy
+++ source/ziphil/controller/PrintController.groovy
@@ -1,13 +1,24 @@
 package ziphil.controller
 
 import groovy.transform.CompileStatic
+import javafx.concurrent.Task
 import javafx.fxml.FXML
+import javafx.scene.Group
+import javafx.scene.Node
+import javafx.scene.Parent
+import javafx.scene.Scene
 import javafx.scene.control.ComboBox
+import javafx.scene.layout.Pane
+import javafx.scene.layout.Region
+import javafx.scene.layout.VBox
+import javafx.print.PageLayout
 import javafx.print.Printer
 import javafx.print.PrinterJob
 import ziphil.custom.Measurement
+import ziphil.custom.SimpleTask
 import ziphil.custom.UtilityStage
 import ziphil.dictionary.Dictionary
+import ziphil.dictionary.Element
 import ziphilib.transform.Ziphilify
 
 
@@ -18,8 +29,10 @@ public class PrintController extends Controller<Void> {
   private static final String TITLE = "印刷"
   private static final Double DEFAULT_WIDTH = -1
   private static final Double DEFAULT_HEIGHT = -1
+  private static final Int MAX_WORD_SIZE = 50
 
   @FXML private ComboBox<Printer> $printerControl
+  private Dictionary $dictionary
   private PrinterJob $printerJob = PrinterJob.createPrinterJob()
 
   public PrintController(UtilityStage<Void> stage) {
@@ -33,6 +46,54 @@ public class PrintController extends Controller<Void> {
   }
 
   public void prepare(Dictionary dictionary) {
+    $dictionary = dictionary
+  }
+
+  @FXML
+  protected void commit() {
+    Task<Void> task = SimpleTask.new() {
+      Pane mainPane = createMainPane()
+      Scene scene = createScene(mainPane)
+      PageLayout layout = $printerJob.getPrinter().getDefaultPageLayout()
+      List<Element> words = $dictionary.getWholeWords()
+      for (Int i = 0 ; i < MAX_WORD_SIZE && i < words.size() ; i ++) {
+        Element word = words[i]
+        Pane pane = word.getContentPaneFactory().create(true)
+        Parent root = scene.getRoot()
+        mainPane.getChildren().add(pane)
+        root.applyCss()
+        root.layout()
+        if (mainPane.getHeight() > layout.getPrintableHeight()) {
+          mainPane.getChildren().remove(pane)
+          $printerJob.printPage(root)
+          mainPane = createMainPane()
+          scene = createScene(mainPane)
+          mainPane.getChildren().add(pane)
+        }
+      }
+      $printerJob.printPage(scene.getRoot())
+      $printerJob.endJob()
+    }
+    Thread thread = Thread.new(task)
+    thread.setDaemon(true)
+    thread.start()
+    $stage.commit(null)
+  }
+
+  private Pane createMainPane() {
+    VBox box = VBox.new(Measurement.rpx(3))
+    URL stylesheetURL = getClass().getClassLoader().getResource("resource/css/main/main.css")
+    PageLayout layout = $printerJob.getPrinter().getDefaultPageLayout()
+    box.setPrefWidth(layout.getPrintableWidth())
+    box.getStylesheets().add(stylesheetURL.toString())
+    return box
+  }
+
+  private Scene createScene(Node node) {
+    Group group = Group.new()
+    Scene scene = Scene.new(group)
+    group.getChildren().add(node)
+    return scene
   }
 
   @FXML
