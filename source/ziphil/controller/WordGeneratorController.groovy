83f178bd1c3a3ac78de4df59b8ca5941943e9b57
Ziphil <yfe23457@nifty.com>
2017-06-22 20:55:03 +0900

    単語自動生成の設定画面で各コントロールの初期化を適切に行うよう変更

--- source/ziphil/controller/WordGeneratorController.groovy
+++ source/ziphil/controller/WordGeneratorController.groovy
@@ -1,13 +1,17 @@
 package ziphil.controller
 
 import groovy.transform.CompileStatic
+import javafx.beans.value.ObservableValue
 import javafx.fxml.FXML
 import javafx.scene.control.ComboBox
 import javafx.scene.control.Spinner
+import javafx.scene.control.TextFormatter
+import ziphil.custom.IntegerUnaryOperator
 import ziphil.custom.Measurement
 import ziphil.custom.StringListEditor
 import ziphil.custom.UtilityStage
 import ziphil.dictionary.EditableDictionary
+import ziphil.dictionary.EquivalentCollectionType
 import ziphilib.transform.Ziphilify
 
 
@@ -22,8 +26,8 @@ public class WordGeneratorController extends Controller<Void> {
   @FXML private StringListEditor $vowelsControl
   @FXML private StringListEditor $consonantsControl
   @FXML private StringListEditor $syllablePatternsControl
-  @FXML private Spinner $minSyllableSizeControl
-  @FXML private Spinner $maxSyllableSizeControl
+  @FXML private Spinner<IntegerClass> $minSyllableSizeControl
+  @FXML private Spinner<IntegerClass> $maxSyllableSizeControl
   @FXML private ComboBox $collectionTypeControl
 
   public WordGeneratorController(UtilityStage<Void> stage) {
@@ -31,7 +35,37 @@ public class WordGeneratorController extends Controller<Void> {
     loadResource(RESOURCE_PATH, TITLE, DEFAULT_WIDTH, DEFAULT_HEIGHT, false)
   }
 
+  @FXML
+  private void initialize() {
+    setupSyllableSizeControls()
+    setupCollectionTypeControl()
+    setupIntegerControls()
+  }
+
   public void prepare(EditableDictionary dictionary) {
+  }
+
+  private void setupSyllableSizeControls() {
+    $minSyllableSizeControl.valueProperty().addListener() { ObservableValue<? extends IntegerClass> observableValue, IntegerClass oldValue, IntegerClass newValue ->
+      if (newValue > $maxSyllableSizeControl.getValue()) {
+        $maxSyllableSizeControl.getValueFactory().setValue(newValue)
+      }
+    }
+    $maxSyllableSizeControl.valueProperty().addListener() { ObservableValue<? extends IntegerClass> observableValue, IntegerClass oldValue, IntegerClass newValue ->
+      if (newValue < $minSyllableSizeControl.getValue()) {
+        $minSyllableSizeControl.getValueFactory().setValue(newValue)
+      }
+    }
+  }
+
+  private void setupCollectionTypeControl() {
+    List<EquivalentCollectionType> collectionTypes = EquivalentCollectionType.getCollectionTypes()
+    $collectionTypeControl.getItems().addAll(collectionTypes)
+  }
+
+  private void setupIntegerControls() {
+    $minSyllableSizeControl.getEditor().setTextFormatter(TextFormatter.new(IntegerUnaryOperator.new()))
+    $maxSyllableSizeControl.getEditor().setTextFormatter(TextFormatter.new(IntegerUnaryOperator.new()))
   }
 
 }
