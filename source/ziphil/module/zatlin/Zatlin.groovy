5a310c1aa55fda78438647cbd0668776de1e682f
Ziphil <yfe23457@nifty.com>
2017-07-16 04:00:08 +0900

    Zatlinの実行のベースとなるクラスを作成

---
+++ source/ziphil/module/zatlin/Zatlin.groovy
@@ -0,0 +1,31 @@
+package ziphil.module.zatlin
+
+import groovy.transform.CompileStatic
+import ziphilib.transform.Ziphilify
+
+
+@CompileStatic @Ziphilify
+public class Zatlin {
+
+  private ZatlinRoot $root
+
+  public void load(File file) {
+    Reader reader = InputStreamReader.new(FileInputStream.new(file), "UTF-8")
+    parse(reader)
+  }
+
+  public void load(String source) {
+    Reader reader = StringReader.new(source)
+    parse(reader)
+  }
+
+  private void parse(Reader reader) {
+    ZatlinParser parser = ZatlinParser.new(reader)
+    $root = parser.readRoot()
+  }
+
+  public String generate() {
+    return $root.generate()
+  }
+
+}
