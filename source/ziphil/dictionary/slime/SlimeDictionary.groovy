681b14b6157cd1254a89fb951ae9909dcd436f7c
Ziphil <yfe23457@nifty.com>
2017-09-16 23:41:48 +0900

    OTM形式の単語が編集されたときにakrantiainの更新を行わないよう変更

--- source/ziphil/dictionary/slime/SlimeDictionary.groovy
+++ source/ziphil/dictionary/slime/SlimeDictionary.groovy
@@ -175,7 +175,6 @@ public class SlimeDictionary extends DictionaryBase<SlimeWord, SlimeSuggestion> 
     updateRegisteredTitles()
     updatePlainInformationTitles()
     updateInformationTitleOrder()
-    updateAkrantiain()
     $changed = true
   }
 
