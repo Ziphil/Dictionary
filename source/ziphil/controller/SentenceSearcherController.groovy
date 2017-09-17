0f68fdda9d8ea6e5702c993e97973b7d774f050f
Ziphil <yfe23457@nifty.com>
2017-09-18 06:21:36 +0900

    文一括検索の画面内に検索結果も表示するよう変更

--- source/ziphil/controller/SentenceSearcherController.groovy
+++ source/ziphil/controller/SentenceSearcherController.groovy
@@ -2,10 +2,12 @@ package ziphil.controller
 
 import groovy.transform.CompileStatic
 import javafx.fxml.FXML
+import javafx.scene.control.ListView
 import javafx.scene.control.TextField
 import javafx.stage.StageStyle
 import javafx.stage.Modality
 import ziphil.custom.Measurement
+import ziphil.custom.SentenceSearchResultCell
 import ziphil.custom.UtilityStage
 import ziphil.dictionary.Dictionary
 import ziphil.dictionary.SentenceSearcher
@@ -17,11 +19,12 @@ public class SentenceSearcherController extends Controller<Void> {
 
   private static final String RESOURCE_PATH = "resource/fxml/controller/sentence_searcher.fxml"
   private static final String TITLE = "文一括検索"
-  private static final Double DEFAULT_WIDTH = Measurement.rpx(480)
-  private static final Double DEFAULT_HEIGHT = -1
+  private static final Double DEFAULT_WIDTH = Measurement.rpx(540)
+  private static final Double DEFAULT_HEIGHT = Measurement.rpx(240)
 
   @FXML private TextField $sentenceControl
   @FXML private TextField $punctuationsControl
+  @FXML private ListView<SentenceSearcher.Result> $resultView
   private Dictionary $dictionary
 
   public SentenceSearcherController(UtilityStage<? super Void> stage) {
@@ -34,16 +37,23 @@ public class SentenceSearcherController extends Controller<Void> {
   }
 
   @FXML
+  public void initialize() {
+    setupResultView()
+  }
+
+  @FXML
   private void search() {
     String sentence = $sentenceControl.getText()
     String punctuations = $punctuationsControl.getText()
     SentenceSearcher searcher = SentenceSearcher.new($dictionary, sentence, punctuations)
-    UtilityStage<Void> nextStage = UtilityStage.new(StageStyle.UTILITY)
-    SentenceSearchResultController controller = SentenceSearchResultController.new(nextStage)
-    nextStage.initModality(Modality.APPLICATION_MODAL)
-    nextStage.initOwner($stage)
-    controller.prepare(searcher.search())
-    nextStage.showAndWait()
+    $resultView.setItems(searcher.search())
+  }
+
+  private void setupResultView() {
+    $resultView.setCellFactory() { ListView<SentenceSearcher.Result> view ->
+      SentenceSearchResultCell cell = SentenceSearchResultCell.new()
+      return cell
+    }
   }
 
 }
