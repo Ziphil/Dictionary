a5ab3247eb92c6c4d9073d76aa9896a6261a40dd
Ziphil <yfe23457@nifty.com>
2017-10-30 21:50:59 +0900

    文一括検索の検索結果に検索した単語名も表示するよう変更

--- source/ziphil/custom/SentenceSearchResultCell.groovy
+++ source/ziphil/custom/SentenceSearchResultCell.groovy
@@ -3,8 +3,11 @@ package ziphil.custom
 import groovy.transform.CompileStatic
 import javafx.geometry.Pos
 import javafx.scene.control.ListCell
+import javafx.scene.control.Separator
 import javafx.scene.layout.Pane
 import javafx.scene.layout.VBox
+import javafx.scene.text.Text
+import ziphil.dictionary.PaneFactory
 import ziphil.dictionary.SentenceSearcher
 import ziphil.dictionary.Word
 import ziphilib.transform.Ziphilify
@@ -19,12 +22,22 @@ public class SentenceSearchResultCell extends ListCell<SentenceSearcher.Result> 
 
   protected void updateItem(SentenceSearcher.Result result, Boolean empty) {
     super.updateItem(result, empty)
-    VBox graphic = VBox.new(Measurement.rpx(3))
+    VBox graphic = VBox.new()
     graphic.prefWidthProperty().bind(getListView().fixedCellSizeProperty().subtract(Measurement.rpx(14)))
     if (!empty && result != null) {
+      Text nameText = Text.new(result.getName())
+      Separator separator = Separator.new()
+      nameText.getStyleClass().add(PaneFactory.CONTENT_CLASS)
+      separator.getStyleClass().addAll(PaneFactory.CONTENT_CLASS, PaneFactory.SEPARATOR_CLASS)
+      graphic.getChildren().addAll(nameText, separator)
       for (Word word : result.getWords()) {
         Pane pane = word.getPlainPaneFactory().create(true)
         graphic.getChildren().add(pane)
+      }
+      if (result.getWords().isEmpty()) {
+        Text emptyText = Text.new("該当なし")
+        emptyText.getStyleClass().add(PaneFactory.CONTENT_CLASS)
+        graphic.getChildren().add(emptyText)
       }
     } 
     setAlignment(Pos.TOP_CENTER)
