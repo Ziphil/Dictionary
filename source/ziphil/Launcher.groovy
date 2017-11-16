e7ff8d4d9f13116004f6804e33bd7596d95a014d
Ziphil <yfe23457@nifty.com>
2017-11-16 20:38:42 +0900

    バージョン番号を変更

--- source/ziphil/Launcher.groovy
+++ source/ziphil/Launcher.groovy
@@ -10,7 +10,7 @@ import ziphilib.transform.Ziphilify
 public class Launcher {
 
   public static final String TITLE = "ZpDIC fetith"
-  public static final Version VERSION = Version.new(1, 24, 0, 2110)
+  public static final Version VERSION = Version.new(1, 25, 0, 2125)
   public static final String FILE_SEPARATOR = createFileSeparator()
   public static final String PATH_SEPARATOR = createPathSeparator()
   public static final String LINE_SEPARATPR = createLineSeparator()
@@ -56,6 +56,9 @@ public class Launcher {
 
 // ◆ Version History
 //
+//  1.25. 0 | PDIC-DIC 形式を読み込む機能を追加。
+//          | OneToMany 形式の単語データをコピーペーストすると不正な関連語データができ得た不具合を修正。
+//          | OneToMany 形式で関連語の相互参照をしようとするとエラーになる不具合を修正。
 //  1.24. 0 | akrantiain 実行ダイアログに辞典に埋め込まれたデータを使うオプションを追加。
 //          | メインウィンドウの単語リストで訳語と内容の間に罫線を表示するよう変更。
 //          | 文一括検索の検索結果の表示方法を変更。
