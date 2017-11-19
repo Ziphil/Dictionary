5afb876789049f9064e509660ddeaca752082643
Ziphil <yfe23457@nifty.com>
2017-11-19 23:07:05 +0900

    シャレイア語辞典のアップロード用のプラグインを実行するクラスを作成

---
+++ source/ziphil/plugin/shaleiaupload/ShaleiaUploadPlugin.groovy
@@ -0,0 +1,44 @@
+package ziphil.plugin.shaleiaupload
+
+import groovy.transform.CompileStatic
+import javafx.scene.image.Image
+import javafx.scene.input.KeyCode
+import ziphil.dictionary.Dictionary
+import ziphil.dictionary.shaleia.ShaleiaDictionary
+import ziphil.plugin.Plugin
+import ziphilib.transform.Ziphilify
+
+
+@CompileStatic @Ziphilify
+public class ShaleiaUploadPlugin implements Plugin {
+
+  private static final String NAME = "データアップロード"
+
+  public void call(Dictionary dictionary) {
+  }
+
+  public Boolean isSupported(Dictionary dictionary) {
+    if (dictionary instanceof ShaleiaDictionary) {
+      if (dictionary.getVersion() == "5.5") {
+        return true
+      } else {
+        return false
+      }
+    } else {
+      return false
+    }
+  }
+
+  public String getName() {
+    return NAME
+  }
+
+  public KeyCode getKeyCode() {
+    return KeyCode.U
+  }
+
+  public Image getIcon() {
+    return null
+  }
+
+}
