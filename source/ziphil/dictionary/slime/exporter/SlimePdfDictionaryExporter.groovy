332dec1b3b249b995d68ccd3a4a17f3740286559
Ziphil <yfe23457@nifty.com>
2017-12-22 02:52:08 +0900

    OTM形式を中間XMLファイルに変換する処理の一部分を作成

--- source/ziphil/dictionary/slime/exporter/SlimePdfDictionaryExporter.groovy
+++ source/ziphil/dictionary/slime/exporter/SlimePdfDictionaryExporter.groovy
@@ -1,8 +1,11 @@
 package ziphil.dictionary.slime.exporter
 
 import groovy.transform.CompileStatic
+import javax.xml.stream.XMLStreamWriter
+import javax.xml.stream.XMLOutputFactory
 import ziphil.dictionary.DictionarySaver
 import ziphil.dictionary.ExportConfig
+import ziphil.dictionary.slime.SlimeEquivalent
 import ziphil.dictionary.slime.SlimeDictionary
 import ziphil.dictionary.slime.SlimeWord
 import ziphilib.transform.Ziphilify
@@ -19,7 +22,62 @@ public class SlimePdfDictionaryExporter extends DictionarySaver<SlimeDictionary>
   }
 
   protected BooleanClass save() {
-    println("Hello")
+    String nextPath = $path.replaceAll(/\.\w+$/, "_temp.xml")
+    saveAsXml(nextPath)
+  }
+
+  protected void saveAsXml(String nextPath) {
+    File file = File.new(nextPath)
+    BufferedWriter bufferedWriter = file.newWriter("UTF-8")
+    XMLOutputFactory factory = XMLOutputFactory.newInstance()
+    XMLStreamWriter writer = factory.createXMLStreamWriter(bufferedWriter)
+    try {
+      writer.writeStartDocument()
+      writer.writeStartElement("words")
+      for (SlimeWord word : $dictionary.getRawWords()) {
+        writeWord(writer, word)
+      }
+      writer.writeEndElement()
+      writer.writeEndDocument()
+    } finally {
+      writer.close()
+      bufferedWriter.close()
+    }
+  }
+
+  private void writeWord(XMLStreamWriter writer, SlimeWord word) {
+    writer.writeStartElement("word")
+    writeEntry(writer, word)
+    writeEquivalents(writer, word)
+    writer.writeEndElement()
+  }
+
+  private void writeEntry(XMLStreamWriter writer, SlimeWord word) {
+    writer.writeStartElement("name")
+    writer.writeCharacters(word.getName())
+    writer.writeEndElement()
+    writer.writeStartElement("id")
+    writer.writeCharacters(word.getId().toString())
+    writer.writeEndElement()
+  }
+
+  private void writeEquivalents(XMLStreamWriter writer, SlimeWord word) {
+    writer.writeStartElement("equivalents")
+    for (SlimeEquivalent equivalent : word.getRawEquivalents()) {
+      writer.writeStartElement("equivalent")
+      writer.writeStartElement("title")
+      writer.writeCharacters(equivalent.getTitle())
+      writer.writeEndElement()
+      writer.writeStartElement("names")
+      for (String name : equivalent.getNames()) {
+        writer.writeStartElement("name")
+        writer.writeCharacters(name)
+        writer.writeEndElement()
+      }
+      writer.writeEndElement()
+      writer.writeEndElement()
+    }
+    writer.writeEndElement()
   }
 
 }
