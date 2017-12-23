c21f79642be9369f3af9e79013b185bed29ee50a
Ziphil <yfe23457@nifty.com>
2017-12-23 14:47:07 +0900

    シャレイア語辞典形式をOTM形式に変換するときにより多くの情報を残すよう変更

--- source/ziphil/dictionary/slime/converter/SlimeShaleiaDictionaryConverter.groovy
+++ source/ziphil/dictionary/slime/converter/SlimeShaleiaDictionaryConverter.groovy
@@ -2,6 +2,7 @@ package ziphil.dictionary.slime.converter
 
 import groovy.transform.CompileStatic
 import java.util.regex.Matcher
+import ziphil.dictionary.AlphabetOrderType
 import ziphil.dictionary.DictionaryLoader
 import ziphil.dictionary.Word
 import ziphil.dictionary.shaleia.ShaleiaDescriptionReader
@@ -10,6 +11,7 @@ import ziphil.dictionary.shaleia.ShaleiaWord
 import ziphil.dictionary.slime.SlimeDictionary
 import ziphil.dictionary.slime.SlimeEquivalent
 import ziphil.dictionary.slime.SlimeInformation
+import ziphil.dictionary.slime.SlimeRelation
 import ziphil.dictionary.slime.SlimeWord
 import ziphilib.transform.Ziphilify
 
@@ -26,6 +28,7 @@ public class SlimeShaleiaDictionaryConverter extends DictionaryLoader<SlimeDicti
 
   protected BooleanClass load() {
     List<ShaleiaWord> sourceWords = $sourceDictionary.getRawWords()
+    Map<String, IntegerClass> sourceIds = HashMap.new()
     Int size = sourceWords.size()
     for (Int i = 0 ; i < size ; i ++) {
       if (isCancelled()) {
@@ -36,9 +39,14 @@ public class SlimeShaleiaDictionaryConverter extends DictionaryLoader<SlimeDicti
         SlimeWord word = SlimeWord.new()
         word.setId(i + 1)
         word.setName(sourceWord.getName())
+        sourceIds.put(sourceWord.getName(), i + 1)
         ShaleiaDescriptionReader sourceReader = ShaleiaDescriptionReader.new(sourceWord.getDescription())
         try {
           while (sourceReader.readLine() != null) {
+            if (sourceReader.findCreationDate()) {
+              String sourceTotalPart = sourceReader.lookupTotalPart()
+              addTotalPart(word, sourceTotalPart)
+            }
             if (sourceReader.findEquivalent()) {
               String sourcePart = sourceReader.lookupPart()
               String sourceEquivalent = sourceReader.lookupEquivalent()
@@ -56,9 +64,35 @@ public class SlimeShaleiaDictionaryConverter extends DictionaryLoader<SlimeDicti
           sourceReader.close()
         }
       }
-      updateProgress(i + 1, size)
+      updateProgress(i + 1, size * 2)
     }
+    for (Int i = 0 ; i < size ; i ++) {
+      if (isCancelled()) {
+        return false
+      }
+      ShaleiaWord sourceWord = sourceWords[i]
+      SlimeWord word = $words[i]
+      ShaleiaDescriptionReader sourceReader = ShaleiaDescriptionReader.new(sourceWord.getDescription())
+      try {
+        while (sourceReader.readLine() != null) {
+          if (sourceReader.findSynonym()) {
+            String sourceSynonym = sourceReader.lookupSynonym()
+            addSynonym(word, sourceSynonym, sourceIds)
+          }
+        }
+      } finally {
+        sourceReader.close()
+      }
+      updateProgress(i + size + 1, size * 2)
+    }
+    $dictionary.setAlphabetOrder($sourceDictionary.getAlphabetOrder())
+    $dictionary.setAlphabetOrderType(AlphabetOrderType.CUSTOM) 
+    $dictionary.setAkrantiainSource($sourceDictionary.getAkrantiainSource())
     return true
+  }
+
+  private void addTotalPart(SlimeWord word, String sourceTotalPart) {
+    word.getTags().add(sourceTotalPart)
   }
 
   private void addEquivalent(SlimeWord word, String sourcePart, String sourceEquivalent) {
@@ -73,6 +107,18 @@ public class SlimeShaleiaDictionaryConverter extends DictionaryLoader<SlimeDicti
     information.setTitle(sourceTitle)
     information.setText(sourceContent.replaceAll(/(\{|\}|\[|\]|\/)/, ""))
     word.getInformations().add(information)
+  }
+
+  private void addSynonym(SlimeWord word, String sourceSynonym, Map<String, IntegerClass> sourceIds) {
+    for (String sourceSynonymName : sourceSynonym.replaceAll(/(\{|\}|\*)/, "").split(/\s*(,|;)\s*/)) {
+      IntegerClass sourceId = sourceIds[sourceSynonymName]
+      if (sourceId != null) {
+        SlimeRelation relation = SlimeRelation.new()
+        relation.setId(sourceId)
+        relation.setName(sourceSynonymName)
+        word.getRelations().add(relation)
+      }
+    }
   }
 
 }
