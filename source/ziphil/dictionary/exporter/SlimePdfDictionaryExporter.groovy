fb65ff10265d02b7860b7d3a5151ae10a8c3948f
Ziphil <yfe23457@nifty.com>
2017-12-25 16:34:17 +0900

    リファクタリング

--- source/ziphil/dictionary/exporter/SlimePdfDictionaryExporter.groovy
+++ source/ziphil/dictionary/exporter/SlimePdfDictionaryExporter.groovy
@@ -1,23 +1,9 @@
 package ziphil.dictionary.exporter
 
 import groovy.transform.CompileStatic
-import java.lang.reflect.InvocationHandler
-import java.lang.reflect.Method
-import java.lang.reflect.Proxy
 import javax.xml.stream.XMLStreamWriter
-import javax.xml.stream.XMLOutputFactory
-import javax.xml.transform.Result
-import javax.xml.transform.Source
 import javax.xml.transform.Transformer
-import javax.xml.transform.TransformerFactory
-import javax.xml.transform.sax.SAXResult
-import javax.xml.transform.stream.StreamResult
-import javax.xml.transform.stream.StreamSource
-import org.apache.fop.apps.FopFactory
-import org.apache.fop.apps.Fop
-import org.apache.fop.apps.MimeConstants
 import ziphil.dictionary.AlphabetOrderType
-import ziphil.dictionary.DictionarySaver
 import ziphil.dictionary.ExportConfig
 import ziphil.dictionary.slime.SlimeEquivalent
 import ziphil.dictionary.slime.SlimeDictionary
@@ -26,16 +12,13 @@ import ziphil.dictionary.slime.SlimeRelation
 import ziphil.dictionary.slime.SlimeVariation
 import ziphil.dictionary.slime.SlimeWord
 import ziphil.module.Setting
-import ziphilib.transform.InnerClass
 import ziphilib.transform.Ziphilify
 
 
 @CompileStatic @Ziphilify
-public class SlimePdfDictionaryExporter extends DictionarySaver<SlimeDictionary> {
+public class SlimePdfDictionaryExporter extends PdfDictionaryExporter<SlimeDictionary> {
 
-  private static final String XSL_PATH = "resource/xsl/slime_pdf.xsl"
-  private static final String CONFIG_PATH = "resource/xsl/config.xml"
-  private static final Boolean USES_SAXON_TRANSFORMER = true
+  private static final String XSLT_PATH = "resource/xsl/slime_pdf.xsl"
 
   private ExportConfig $config
 
@@ -44,85 +27,20 @@ public class SlimePdfDictionaryExporter extends DictionarySaver<SlimeDictionary>
     $config = config
   }
 
-  protected BooleanClass save() {
-    saveTemporary()
-    updateProgress(1, 2)
-    transformDebug()
-    transform()
-    updateProgress(2, 2)
-  }
-
-  protected BooleanClass transform() {
-    File file = File.new($path)
-    File temporaryFile = File.new($path.replaceAll(/\.\w+$/, "_temp.xml"))
-    BufferedOutputStream stream = file.newOutputStream()
-    BufferedInputStream temporaryStream = temporaryFile.newInputStream()
-    try {
-      FopFactory fopFactory = FopFactory.newInstance(File.new(".").toURI(), getClass().getClassLoader().getResourceAsStream(CONFIG_PATH))
-      Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, stream)
-      Result result = SAXResult.new(fop.getDefaultHandler())
-      Source source = StreamSource.new(temporaryStream)
-      Transformer transformer = createTransformer()
-      transformer.transform(source, result)
-    } finally {
-      stream.close()
-      temporaryStream.close()
-    }
-  }
-
-  protected BooleanClass transformDebug() {
-    File file = File.new($path.replaceAll(/\.\w+$/, "_fo.fo"))
-    File temporaryFile = File.new($path.replaceAll(/\.\w+$/, "_temp.xml"))
-    BufferedOutputStream stream = file.newOutputStream()
-    BufferedInputStream temporaryStream = temporaryFile.newInputStream()
-    try {
-      Result result = StreamResult.new(stream)
-      Source source = StreamSource.new(temporaryStream)
-      Transformer transformer = createTransformer()
-      transformer.transform(source, result)
-    } finally {
-      stream.close()
-      temporaryStream.close()
-    }
-  }
-
-  private Transformer createTransformer() {
-    Setting setting = Setting.getInstance()
-    Source xslSource = StreamSource.new(getClass().getClassLoader().getResourceAsStream(XSL_PATH))
-    TransformerFactory factory = (USES_SAXON_TRANSFORMER) ? TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", null) : TransformerFactory.newInstance()
-    Transformer transformer = factory.newTransformer(xslSource)
-    transformer.setParameter("punctuation", ", ")
-    transformer.setParameter("variation-marker", setting.getVariationMarker())
-    transformer.setParameter("relation-marker", setting.getRelationMarker())
-    return transformer
-  }
-
-  protected BooleanClass saveTemporary() {
-    File file = File.new($path.replaceAll(/\.\w+$/, "_temp.xml"))
-    BufferedWriter bufferedWriter = file.newWriter("UTF-8")
-    XMLOutputFactory factory = XMLOutputFactory.newInstance()
-    XMLStreamWriter rawWriter = factory.createXMLStreamWriter(bufferedWriter)
-    PrettyPrintHandler handler = PrettyPrintHandler.new(rawWriter)
-    XMLStreamWriter writer = (XMLStreamWriter)Proxy.newProxyInstance(XMLStreamWriter.getClassLoader(), (Class[])[XMLStreamWriter].toArray(), handler)
-    try {
-      writer.writeStartDocument()
-      writer.writeStartElement("words")
-      String beforeInitialLetter = ""
-      for (SlimeWord word : $dictionary.getRawSortedWords()) {
-        String initialLetter = calculateInitialLetter(word.getName())
-        if (initialLetter != beforeInitialLetter) {
-          beforeInitialLetter = initialLetter
-          writeCaption(writer, initialLetter)
-        }
-        writeWord(writer, word)
+  protected void writeTemporary(XMLStreamWriter writer) {
+    writer.writeStartDocument()
+    writer.writeStartElement("words")
+    String beforeInitialLetter = ""
+    for (SlimeWord word : $dictionary.getRawSortedWords()) {
+      String initialLetter = calculateInitialLetter(word.getName())
+      if (initialLetter != beforeInitialLetter) {
+        beforeInitialLetter = initialLetter
+        writeCaption(writer, initialLetter)
       }
-      writer.writeEndElement()
-      writer.writeEndDocument()
-    } finally {
-      writer.close()
-      bufferedWriter.close()
+      writeWord(writer, word)
     }
-    return true
+    writer.writeEndElement()
+    writer.writeEndDocument()
   }
 
   private String calculateInitialLetter(String name) {
@@ -262,54 +180,15 @@ public class SlimePdfDictionaryExporter extends DictionarySaver<SlimeDictionary>
     writer.writeEndElement()
   }
 
-}
-
-
-@InnerClass(SlimePdfDictionaryExporter)
-@CompileStatic @Ziphilify
-private static class PrettyPrintHandler implements InvocationHandler {
-
-  private XMLStreamWriter $target
-  private Int $depth = 0
-  private Map<IntegerClass, BooleanClass> $childFlags = HashMap.new()
-
-  public PrettyPrintHandler(XMLStreamWriter target) {
-    $target = target
+  protected void setupTransformer(Transformer transformer) {
+    Setting setting = Setting.getInstance()
+    transformer.setParameter("punctuation", ", ")
+    transformer.setParameter("variation-marker", setting.getVariationMarker())
+    transformer.setParameter("relation-marker", setting.getRelationMarker())
   }
 
-  public Object invoke(Object proxy, Method method, Object[] args) {
-    String name = method.getName()
-    if (name == "writeStartElement") {
-      if ($depth > 0) {
-        $childFlags[$depth - 1] = true
-      }
-      $childFlags[$depth] = false
-      $target.writeCharacters("\n")
-      $target.writeCharacters(repeat($depth, "  "))
-      $depth ++
-    } else if (name == "writeEndElement") {
-      $depth --
-      if ($childFlags[$depth]) {
-        $target.writeCharacters("\n")
-        $target.writeCharacters(repeat($depth, "  "))
-      }
-    } else if (name == "writeEmptyElement") {
-      if ($depth > 0) {
-        $childFlags[$depth - 1] = true
-      }
-      $target.writeCharacters("\n")
-      $target.writeCharacters(repeat($depth, "  "))
-    }
-    method.invoke($target, args)
-    return null
-  }
-
-  private String repeat(Int depth, String string) {
-    String result = ""
-    while (depth -- > 0) {
-      result += string
-    }
-    return result
+  protected String getXsltPath() {
+    return XSLT_PATH
   }
 
 }
