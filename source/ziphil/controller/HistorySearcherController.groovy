556702c47f2d4e235796f2dcb5e374948bbae305
Ziphil <yfe23457@nifty.com>
2018-06-08 17:53:52 +0900

    検索履歴から検索するウィンドウのサイズを変更

--- source/ziphil/controller/HistorySearcherController.groovy
+++ source/ziphil/controller/HistorySearcherController.groovy
@@ -15,7 +15,7 @@ public class HistorySearcherController extends Controller<SearchParameter> {
 
   private static final String RESOURCE_PATH = "resource/fxml/controller/history_searcher.fxml"
   private static final String TITLE = "検索履歴から検索"
-  private static final Double DEFAULT_WIDTH = Measurement.rpx(240)
+  private static final Double DEFAULT_WIDTH = Measurement.rpx(360)
   private static final Double DEFAULT_HEIGHT = Measurement.rpx(360)
 
   @FXML private ListView<SearchParameter> $parameterView
