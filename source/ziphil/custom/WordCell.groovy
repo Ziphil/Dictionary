38fbaae4d14937901d5774f1429d8c95d1d4901f
Ziphil <yfe23457@nifty.com>
2018-07-12 01:15:23 +0900

    マーカーの有無に応じてセルのスタイルクラスを書き換えるよう変更

--- source/ziphil/custom/WordCell.groovy
+++ source/ziphil/custom/WordCell.groovy
@@ -3,8 +3,10 @@ package ziphil.custom
 import groovy.transform.CompileStatic
 import javafx.scene.control.ListCell
 import javafx.scene.layout.Pane
+import ziphil.dictionary.BadgeType
 import ziphil.dictionary.Element
 import ziphil.dictionary.IndividualSetting
+import ziphil.dictionary.Word
 import ziphilib.transform.Ziphilify
 
 
@@ -26,6 +28,18 @@ public class WordCell extends ListCell<Element> {
     } else {
       Pane graphic = word.getPaneFactory().create(false)
       graphic.prefWidthProperty().bind(getListView().widthProperty().subtract(Measurement.rpx(29)))
+      if (word instanceof Word) {
+        Int styleClassSize = getStyleClass().size()
+        for (int i = 0 ; i < styleClassSize - 3 ; i ++) {
+          getStyleClass().removeLast()
+        }
+        for (Map.Entry<BadgeType, Set<String>> entry : $individualSetting.getBadgedIdentifiers()) {
+          BadgeType type = entry.getKey()
+          if (entry.getValue().contains(word.getIdentifier())) {
+            getStyleClass().add(type.getStyleClass())
+          }
+        }
+      }
       setText(null)
       setGraphic(graphic)
     }
--- source/ziphil/dictionary/BadgeType.groovy
+++ source/ziphil/dictionary/BadgeType.groovy
@@ -12,18 +12,20 @@ import ziphilib.transform.Ziphilify
 @CompileStatic @Ziphilify
 public enum BadgeType {
 
-  CIRCLE("マーク1", "circle"),
-  SQUARE("マーク2", "square"),
-  UP_TRIANGLE("マーク3", "up_triangle"),
-  DOWN_TRIANGLE("マーク4", "down_triangle")
+  CIRCLE("マーク1", "circle-badge", "circle"),
+  SQUARE("マーク2", "square-badge", "square"),
+  UP_TRIANGLE("マーク3", "up-triangle-badge", "up_triangle"),
+  DOWN_TRIANGLE("マーク4", "down-triangle-badge", "down_triangle")
 
   private static final String IMAGE_DIRECTORY = "resource/image/badge/"
 
   private String $name
+  private String $styleClass
   private String $path
 
-  private BadgeType(String name, String path) {
+  private BadgeType(String name, String styleClass, String path) {
     $name = name
+    $styleClass = styleClass
     $path = path
   }
 
@@ -33,8 +35,10 @@ public enum BadgeType {
 
   public static Node createImageNode() {
     HBox box = HBox.new(Measurement.rpx(3))
+    box.getStyleClass().add("badge-container")
     for (BadgeType type : BadgeType.values()) {
       ImageView view = ImageView.new(type.createImage())
+      view.getStyleClass().add(type.getStyleClass())
       box.getChildren().add(view)
     }
     return box
@@ -42,6 +46,10 @@ public enum BadgeType {
 
   public String getName() {
     return $name
+  }
+
+  public String getStyleClass() {
+    return $styleClass
   }
 
 }
