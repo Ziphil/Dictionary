a98407da294d9eea8c753162edcfd5bbefadfb11
Ziphil <yfe23457@nifty.com>
2018-11-02 23:08:35 +0900

    PDIC-DIC形式をPDIC-CSV形式に変換できるよう修正

--- source/ziphil/dictionary/converter/PersonalConverterFactory.groovy
+++ source/ziphil/dictionary/converter/PersonalConverterFactory.groovy
@@ -4,7 +4,9 @@ import groovy.transform.CompileStatic
 import ziphil.dictionary.ConverterFactory
 import ziphil.dictionary.Dictionary
 import ziphil.dictionary.DictionaryFactory
+import ziphil.dictionary.IdentityConverter
 import ziphil.dictionary.Loader
+import ziphil.dictionary.personal.BinaryDictionary
 import ziphil.dictionary.personal.PersonalDictionary
 import ziphil.dictionary.shaleia.ShaleiaDictionary
 import ziphil.dictionary.slime.SlimeDictionary
@@ -17,7 +19,9 @@ public class PersonalConverterFactory extends ConverterFactory {
   public Loader create(DictionaryFactory factory, Dictionary sourceDictionary) {
     Loader converter = null
     if (factory.getDictionaryClass() == PersonalDictionary) {
-      if (sourceDictionary instanceof ShaleiaDictionary) {
+      if (sourceDictionary instanceof BinaryDictionary) {
+        converter = IdentityConverter.new((Dictionary)sourceDictionary)
+      } else if (sourceDictionary instanceof ShaleiaDictionary) {
         converter = PersonalShaleiaConverter.new(sourceDictionary)
       } else if (sourceDictionary instanceof SlimeDictionary) {
         converter = PersonalSlimeConverter.new(sourceDictionary)
@@ -28,7 +32,9 @@ public class PersonalConverterFactory extends ConverterFactory {
 
   public Boolean isAvailable(DictionaryFactory factory, Dictionary sourceDictionary) {
     if (factory.getDictionaryClass() == PersonalDictionary) {
-      if (sourceDictionary instanceof ShaleiaDictionary) {
+      if (sourceDictionary instanceof BinaryDictionary) {
+        return true
+      } else if (sourceDictionary instanceof ShaleiaDictionary) {
         return true
       } else if (sourceDictionary instanceof SlimeDictionary) {
         return true
